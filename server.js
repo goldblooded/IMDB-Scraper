d2c770d310838b70e6bc4555d67605f6b9be68bf
Jay Bittner <jtbitt@gmail.com>
2015-08-14 03:27:37 -0700

    Added code to get title

--- server.js
+++ server.js
@@ -5,27 +5,33 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
-  // The URL we are scraping from - in this case Anchorman 2.
-
-  url = 'http://www.imdb.com/title/tt1229340/';
-
-  // The structure of our request call
-  // The first parameter is our URL
-  // The callback function takes 3 paramaters, an error, response status code and the html
+  
+  url = 'http://www.imdb.com/title/tt0103064/';
 
   request(url, function(error, response, html){
-
-  	// Check to make sure no errors occurred when making the request
-
-  	if(!error){
-  		// Utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
+    if(!error){
   		var $ = cheerio.load(html);
-
-  		// Variables we're going to capture
 
   		var title, release, rating;
   		var json = { title: "", release : "", rating : ""};
+
+  		// We'll use the unique header class as a starting point.
+
+  		$('.header').filter(function({
+        
+        // Let's store the data we filter into a variable so we can easily see what's going on.
+
+        var data = $(this);
+
+        // In examining the DOM we notice that the title rests within the first child element of the header tag.
+        // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+        title = data.children().first().text();
+
+        // Once we have our title, we'll store it to the our json object.
+
+        json.title = title;
+  		})
   	}
   })
 })
