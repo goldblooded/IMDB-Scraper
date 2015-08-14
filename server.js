ccc430603fea7eff23c2f97e6562079bab5a9c53
Jay Bittner <jtbitt@gmail.com>
2015-08-14 02:47:09 -0700

    Added Scraper Logic

--- server.js
+++ server.js
@@ -5,9 +5,29 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
+  // The URL we are scraping from - in this case Anchorman 2.
 
-	// All the web scraping magic will happen here
+  url = 'http://www.imdb.com/title/tt1229340/';
 
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 paramaters, an error, response status code and the html
+
+  request(url, function(error, response, html){
+
+  	// Check to make sure no errors occurred when making the request
+
+  	if(!error){
+  		// Utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+  		var $ = cheerio.load(html);
+
+  		// Variables we're going to capture
+
+  		var title, release, rating;
+  		var json = { title: "", release : "", rating : ""};
+  	}
+  })
 })
 
 app.listen('8081')
