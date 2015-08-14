98bdc88335fea83db1f1ddb01aff935575c2c9cd
Jay Bittner <jtbitt@gmail.com>
2015-08-14 03:37:56 -0700

    Added code to get release date

--- server.js
+++ server.js
@@ -15,22 +15,15 @@ app.get('/scrape', function(req, res){
   		var title, release, rating;
   		var json = { title: "", release : "", rating : ""};
 
-  		// We'll use the unique header class as a starting point.
-
   		$('.header').filter(function({
-        
-        // Let's store the data we filter into a variable so we can easily see what's going on.
 
         var data = $(this);
 
-        // In examining the DOM we notice that the title rests within the first child element of the header tag.
-        // Utilizing jQuery we can easily navigate and get the text by writing the following code:
-
         title = data.children().first().text();
-
-        // Once we have our title, we'll store it to the our json object.
+        release = data.children().last().children
 
         json.title = title;
+        json.release = release;
   		})
   	}
   })
