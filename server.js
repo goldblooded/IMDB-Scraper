32d99f3253cb7f397a62525c18909807ec2919df
Jay Bittner <jtbitt@gmail.com>
2015-08-16 04:09:40 -0700

    Fixed ugly spacing

--- server.js
+++ server.js
@@ -6,38 +6,40 @@ var app     = express();
 
 app.get('/scrape', function(req, res){
 	// Let's scrape Terminator 2
-	url = 'http://www.imdb.com/title/tt0103064/';
+  url = 'http://www.imdb.com/title/tt0103064/';
 
-	request(url, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
 
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+      var title, release, rating;
+      var json = { title : "", release : "", rating : ""};
 
-			$('.header').filter(function(){
-		    var data = $(this);
-		    title = data.children().first().text();
-		    release = data.children().last().children().text();
+      $('.header').filter(function(){
+        var data = $(this);
+        title = data.children().first().text();
+        release = data.children().last().children().text();
 
-		    json.title = title;
-		    json.release = release;
-	    })
+        json.title = title;
+        json.release = release;
+      })
 
-	    $('.star-box-giga-star').filter(function(){
-	      var data = $(this);
-	      rating = data.text();
+      $('.star-box-giga-star').filter(function(){
+        var data = $(this);
+        rating = data.text();
 
-	      json.rating = rating;
-	    })
-		}
+        json.rating = rating;
+      })
+    }
 
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+    console.log(json);
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
       console.log('File successfully written! - Check your project directory for the output.json file');
-	  })
+    })
 
     res.send('Check your console!')
-	})
+  })
 })
 
 app.listen('8081')
